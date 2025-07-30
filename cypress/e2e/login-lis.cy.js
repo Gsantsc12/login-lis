27852b4fd2ae68c9f2119c5c964adf34cf432d1d
Gabriel Ferreira Santos <gabriel.santos@pierserv.com.br>
2025-07-30 15:27:26 -0300

    Login-Lis

---
+++ cypress/e2e/login-lis.cy.js
@@ -0,0 +1,29 @@
+describe('Fluxo de login', () => {
+  it('Deve realizar login com sucesso', () => {
+    cy.visit('https://hlis.pslogistica.com.br/login/'); // Altere para a URL real
+
+    // Inserçao das credencias
+    cy.get(':nth-child(1) > #search').type('gabriel.santos');
+    cy.get(':nth-child(2) > #search').type('12345');
+    cy.get('.box_fields > div.w-100 > .custom-button-login').click();
+    // Seleciona o cliente, base e divisão
+      cy.get('#cliente')
+      .should('be.visible' ,'Selecionar cliente')
+      .select('HNK - CKBR BEBIDAS LTDA')
+      .should('have.value', '57');
+
+      cy.get('#base')
+      .should('be.visible' ,'Selecionar Base')
+      .select('Matriz PierServ')
+      .should('have.value', '1');// Confirma se foi selecionado corretamente
+
+      cy.get('#divisao')
+      .should('be.visible' ,'Selecionar Base')
+      .select('CKBR BEBIDAS LTDA')
+      .should('have.value', '66');
+
+      cy.get('.d-flex > .btn').click();
+
+
+  });
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,6 @@
+// Cypress.Commands.add('login', (email, senha) => {
+//   cy.visit('/login');
+//   cy.get('input[name="email"]').type(email);
+//   cy.get('input[name="senha"]').type(senha);
+//   cy.get('button[type="submit"]').click();
+// });
