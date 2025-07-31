735a58b03e3dd754fb031f843c7c117df6b640a2
Gabriel Ferreira Santos <gabriel.santos@pierserv.com.br>
2025-07-31 17:02:37 -0300

    Ajuste final

--- cypress/e2e/Cadastro-clientes.cy.js
+++ cypress/e2e/Cadastro-clientes.cy.js
@@ -1,51 +1,74 @@
-const { TIMEOUT } = require("dns");
-
 describe('Cadastro de Usuarios', () => {
   it('Deve realizar login com sucesso', () => {
-    cy.visit('https://hlis.pslogistica.com.br/login/'); // Altere para a URL real
+    cy.visit('https://hlis.pslogistica.com.br/login/');
 
-    // Inserçao das credencias
+    // Inserção das credenciais
     cy.get(':nth-child(1) > #search').type('gabriel.santos');
     cy.get(':nth-child(2) > #search').type('12345');
     cy.get('.box_fields > div.w-100 > .custom-button-login').click();
+
     // Seleciona o cliente, base e divisão
-      cy.get('#cliente')
-      .should('be.visible' ,'Selecionar cliente')
+    cy.get('#cliente')
+      .should('be.visible')
       .select('HNK - CKBR BEBIDAS LTDA')
       .should('have.value', '57');
 
-      cy.get('#base')
-      .should('be.visible' ,'Selecionar Base')
+    cy.get('#base')
+      .should('be.visible')
       .select('Matriz PierServ')
-      .should('have.value', '1');// Confirma se foi selecionado corretamente
+      .should('have.value', '1');
 
-      cy.get('#divisao')
-      .should('be.visible' ,'Selecionar Base')
+    cy.get('#divisao')
+      .should('be.visible')
       .select('CKBR BEBIDAS LTDA')
       .should('have.value', '66');
-      cy.get('.d-flex > .btn').click();
 
-      cy.get(':nth-child(5) > .link_menu > #title_menu').click();
-      cy.get('.active > .submenu > :nth-child(2) > a').click()
+    cy.get('.d-flex > .btn').click();
 
-      cy.contains('a.btn.btn-pier', 'Cadastrar').click({ force: true });
+    cy.get(':nth-child(5) > .link_menu > #title_menu').click();
+    cy.get('.active > .submenu > :nth-child(2) > a').click();
 
-      cy.get('#sigla_cliente').click().type('KKK')
+    cy.contains('a.btn.btn-pier', 'Cadastrar').click({ force: true });
 
-      cy.get('.form-input > .btn').click();
+    cy.get('#sigla_cliente').click().type('rrr');
+    cy.get('.form-input > .btn').click();
 
-      cy.get('#select-galpao')
-      .should('be.visible' ,'Selecione')
+    cy.get('#select-galpao')
+      .should('be.visible')
       .select('1')
       .should('have.value', '1');
 
-      cy.wait(4800);
+    cy.wait(2000);
 
-      cy.contains('#btn-adicionar-galpao', 'Adicionar').click({ force: true });
+    cy.contains('#btn-adicionar-galpao', 'Adicionar').click({ force: true });
 
-      cy.get('#nome_divisao_input').type('548754');
+    cy.get('#nome_divisao_input').type('548754');
 
-      cy.get('#btnSalvarCliente').click();
+    cy.get('.col-md-8 > [data-bs-toggle="modal"]').click();
+    cy.get('#cnpj_pesq_cliente').type('36742194000148');
+    cy.contains('#btn_carreg_infos_cliente', 'Pesquisar').click({ force: true });
 
-})
-})
+    cy.get('#body_pessoas_tabela > tr > :nth-child(1)').click();
+
+    // Campo linhas_separacao[1]
+    cy.get('input[name="linhas_separacao[1]"]')
+      .scrollIntoView()
+      .should('exist')
+      .type('50', { force: true });
+
+    // Taxa Administrativa
+    cy.get('input[name="taxa_administrativa[1]"]').type('1', { force: true });
+
+    // SLA Recebimento
+    cy.get('input[name="sla_recebimento[1]"]').type('1', { force: true });
+
+    // SLA Retorno Evento
+    cy.get('input[name="sla_retorno_evento[1]"]').type('1', { force: true });
+
+    // Salvar cliente
+    cy.get('#btnSalvarCliente').click({ force: true });
+
+    // Validação final
+    cy.contains('Cliente cadastrado com sucesso!').should('be.visible');
+  });
+});
