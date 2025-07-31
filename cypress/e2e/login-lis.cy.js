8676dcac8c38c3075d8bd8dce06b98712a031d89
Gabriel Ferreira Santos <gabriel.santos@pierserv.com.br>
2025-07-31 16:19:26 -0300

    Ajuste

--- cypress/e2e/login-lis.cy.js
+++ cypress/e2e/login-lis.cy.js
@@ -4,23 +4,24 @@ describe('Fluxo de Cadastro de item', () => {
 
     // Inserçao das credencias
     cy.get(':nth-child(1) > #search').type('gabriel.santos');
-    cy.get(':nth-child(2) > #search').type('12345');
-    cy.get('.box_fields > div.w-100 > .custom-button-login').click();
+      cy.get(':nth-child(2) > #search').type('12345');
+        cy.get('.box_fields > div.w-100 > .custom-button-login').click();
     // Seleciona o cliente, base e divisão
       cy.get('#cliente')
-      .should('be.visible' ,'Selecionar cliente')
-      .select('HNK - CKBR BEBIDAS LTDA')
-      .should('have.value', '57');
+        .should('be.visible' ,'Selecionar cliente')
+          .select('HNK - CKBR BEBIDAS LTDA')
+            .should('have.value', '57');
 
       cy.get('#base')
-      .should('be.visible' ,'Selecionar Base')
-      .select('Matriz PierServ')
-      .should('have.value', '1');// Confirma se foi selecionado corretamente
+        .should('be.visible' ,'Selecionar Base')
+          .select('Matriz PierServ')
+            .should('have.value', '1');// Confirma se foi selecionado corretamente
 
       cy.get('#divisao')
-      .should('be.visible' ,'Selecionar Base')
-      .select('CKBR BEBIDAS LTDA')
-      .should('have.value', '66');
+        .should('be.visible' ,'Selecionar Base')
+          .select('CKBR BEBIDAS LTDA')
+            .should('have.value', '66');
+
       cy.get('.d-flex > .btn').click();
 
   });
